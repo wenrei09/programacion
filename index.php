95280b4d8f6ad59d200e6e2812d02c3c52e2686b
ramoncamacho <wenrei09@gmail.com>
2017-03-16 19:11:05 +0100

    anadido envio de post de PHP

--- index.php
+++ index.php
@@ -2,9 +2,20 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
 </head>
 <body>
-	
+	<?php
+	$nombre= "Ramon";
+	if (isset($_POST ['nombre'])) {
+		$nombre=$_POST['nombre'];
+	}
+
+echo "hola". $nombre;
+	?>
+<form action="index.php" method="post">
+	<input type="text" name="nombre" placeholder="escriba tu nombre">
+	<br>
+	<input type="submit" value="enviar">
+	</form>
 </body>
 </html>
